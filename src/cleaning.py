17eca4b7781c01d9473b1afa3bb46d98e959ce58
nelnsc <singcheuk2@gmail.com>
2023-09-16 02:05:06 +0100

    need to fix ['DataFrame' object has no attribute 'map']

--- src/cleaning.py
+++ src/cleaning.py
@@ -8,6 +8,7 @@ DATA_DIR = os.path.join(PWD, "../data/")
 
 ffill_columns = ["Date", "Exercise", "Variation", "Weight"]
 
+
 def main():
     df = pd.read_csv(os.path.join(DATA_DIR, "data.csv"), index_col=0)
     # convert to upper case
@@ -16,12 +17,13 @@ def main():
     df = df.map(lambda x: x.rstrip() if isinstance(x, str) else x)
     # fill 0 weight for selected Exercises
     # I was too lazy to fill in 0 when working out
-    df.loc[(~df['Exercise'].isnull()) & (df["Weight"].isnull()), ['Weight']] = 0
+    df.loc[(~df["Exercise"].isnull()) & (df["Weight"].isnull()), ["Weight"]] = 0
     # forward fill
     [df[col].ffill(inplace=True) for col in ffill_columns]
     # convert str to datetime
-    df["Date"] = pd.to_datetime(df["Date"], dayfirst=True, errors='coerce')
+    df["Date"] = pd.to_datetime(df["Date"], dayfirst=True, errors="coerce")
     df.to_csv(os.path.join(DATA_DIR, "clean.csv"), index=False)
+
 
 if __name__ == "__main__":
     main()
